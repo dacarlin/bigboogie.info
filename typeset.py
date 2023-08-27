2b4880e2a7aa15655e5b23895594b9d136777eff
Alex Carlin <alxcarln@gmail.com>
2023-08-27 14:10:58 -0700

    Add idea for automating typesetting

---
+++ typeset.py
@@ -0,0 +1,17 @@
+# Idea: have a Python script typeset the blog 
+#
+# python typeset.py index.md index.html 
+#
+# generates index.html from index.md, which is just the blog 
+# in Markdown
+#
+# example blog post 
+#
+# ```
+# # Song in key of A
+#
+# The text and stuff here 
+# 
+# <canvas alt="123456 E"></canvas>
+#
+# I believe will just work with Markdown. Anyway that's the idea 
